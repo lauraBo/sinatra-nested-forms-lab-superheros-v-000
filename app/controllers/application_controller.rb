fcc9afa16b366d87528672027fa4bfe57c3dec08
Laura Bodell <bodelllaura@gmail.com>
2018-05-07 21:08:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ end
 
 post '/teams' do
   
-  @team_name = team[:name]
+  @team_name = params[:team][:name]
+  @team_motto = params[:team][:motto]
+  @members = params[:team][:members]
   erb :super_hero
 end
 
