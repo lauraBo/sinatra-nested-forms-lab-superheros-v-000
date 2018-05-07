2fb54de127b38bab48c95ba100775e4f1057c134
Laura Bodell <bodelllaura@gmail.com>
2018-05-07 21:20:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   
   @team_name = params[:team][:name]
   @team_motto = params[:team][:motto]
-  @members = params[:team][:members]
+  @team_members = params[:team][:members]
   
   erb :super_hero
  end
